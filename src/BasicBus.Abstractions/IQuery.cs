a03d6be1ba0c8740c0fcf0bca7ef56a0b9cb3d7d
Mohammad ShafieZadeh <m.shafiezadeh@outlook.com>
2020-12-04 03:49:20 +0330

    initial project setup

---
+++ src/BasicBus.Abstractions/IQuery.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicBus.Abstractions
+{
+    /// <summary>
+    /// Represents a query that is intended to return data
+    /// </summary>
+    public interface IQuery<in TQueryResult>
+    {
+        
+    }
+}
