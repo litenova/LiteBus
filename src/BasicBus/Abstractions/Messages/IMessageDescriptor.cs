bccf38dc0fa0c50a5ad320ac066a60d15075446a
A. Shafie <Litenova@outlook.com>
2020-12-04 23:02:09 +0330

    add message abstractions

---
+++ src/BasicBus/Abstractions/Messages/IMessageDescriptor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BasicBus.Abstractions
+{
+    public interface IMessageDescriptor
+    {
+        Type MessageType { get; }
+        Type HandlerType { get; }
+    }
+}
---
+++ src/BasicBus/Builders/QueryMediatorBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BasicBus.Abstractions;
+using BasicBus.Internal;
+
+namespace BasicBus.Builders
+{
+    public class QueryMediatorBuilder
+    {
+        public IQueryMediator Build(IServiceProvider serviceProvider, 
+                                    IMessageRegistry messageRegistry)
+        {
+            return new QueryMediator(serviceProvider, messageRegistry);
+        }
+    }
+}
