261f455fb1b67198e8e7faf07b672af502670073
Mohammad ShafieZadeh <m.shafiezadeh@outlook.com>
2021-07-08 11:14:38 +0430

    add cancellation token to query mediator and event mediator

--- src/LiteBus.Messaging.Abstractions/ISyncMessageHandler.cs
+++ src/LiteBus.Messaging.Abstractions/ISyncMessageHandler.cs
@@ -1,7 +1,4 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-
-namespace LiteBus.Messaging.Abstractions
+﻿namespace LiteBus.Messaging.Abstractions
 {
     /// <summary>
     ///     The base of all synchronous message handlers
