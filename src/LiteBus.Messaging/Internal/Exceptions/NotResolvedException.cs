87424fde0ee908f301fb0be5196f34b138cb217e
Mohammad ShafieZadeh <m.shafiezadeh@outlook.com>
2021-08-01 11:38:08 +0430

    improve message context

---
+++ src/LiteBus.Messaging/Internal/Exceptions/NotResolvedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace LiteBus.Messaging.Internal.Exceptions
+{
+    [Serializable]
+    public class NotResolvedException : Exception
+    {
+        public NotResolvedException(Type type) : base($"The type of '{type.Name}' could not be resolved")
+        {
+        }
+    }
+}
--- src/LiteBus.Messaging/Internal/Mediator/MessageContext.cs
+++ src/LiteBus.Messaging/Internal/Mediator/MessageContext.cs
@@ -1,25 +1,106 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using LiteBus.Messaging.Abstractions;
+using LiteBus.Messaging.Internal.Exceptions;
 using LiteBus.Messaging.Internal.Extensions;
 
 namespace LiteBus.Messaging.Internal.Mediator
 {
+    // A handler Handles a <Message> and returns <Message Result> 
+    
+    // Message -> Mediator -> Message -> 
+    
+    // Message 
+    // Handler
+    // 
+    
     public class MessageContext<TMessage, TMessageResult> : IMessageContext<TMessage, TMessageResult>
     {
+        private readonly IServiceProvider _serviceProvider;
+
         public MessageContext(IMessageDescriptor descriptor, IServiceProvider serviceProvider)
         {
-            Handlers = descriptor.PostHandleHookDescriptors
-                                 .OrderBy(d => d.Order)
-                                 .Select(h => new Lazy<IMessageHandler<TMessage, TMessageResult>>(() =>
-                                             serviceProvider.GetService(h.HookType) as
-                                                 IMessageHandler<TMessage, TMessageResult>))
-                                 .ToLazyReadOnlyCollection();
+            _serviceProvider = serviceProvider;
+
+            Handlers = ResolveHandlers(descriptor.HandlerTypes).ToLazyReadOnlyCollection();
+            PostHandleHooks = ResolvePostHandleHooks(descriptor.PostHandleHookDescriptors).ToLazyReadOnlyCollection();
+            PreHandleHooks = ResolvePreHandleHooks(descriptor.PreHandleHookDescriptors).ToLazyReadOnlyCollection();
+        }
+
+        private IEnumerable<Lazy<IMessageHandler<TMessage, TMessageResult>>> ResolveHandlers(
+            IEnumerable<Type> handlerTypes)
+        {
+            foreach (var handlerType in handlerTypes)
+            {
+                var resolveFunc = new Func<object?>(() => _serviceProvider.GetService(handlerType));
+
+                yield return new(() =>
+                {
+                    var handler = resolveFunc();
+
+                    if (handler is null)
+                    {
+                        throw new NotResolvedException(handlerType);
+                    }
+
+                    return null;
+                });
+            }
+        }
+
+        private IEnumerable<Lazy<IPreHandleHook<TMessage>>> ResolvePreHandleHooks(
+            IReadOnlyCollection<IHookDescriptor> descriptors)
+        {
+            foreach (var descriptor in descriptors)
+            {
+                var hookType = descriptor.HookType;
+
+                var resolveFunc = new Func<object?>(() => _serviceProvider.GetService(hookType));
+
+                yield return new(() =>
+                {
+                    var hook = resolveFunc();
+
+                    if (hook is null)
+                    {
+                        throw new NotResolvedException(hookType);
+                    }
+
+                    var result = hook as IPreHandleHook<TMessage>;
+
+                    return result;
+                });
+            }
+        }
+
+        private IEnumerable<Lazy<IPostHandleHook<TMessage>>> ResolvePostHandleHooks(
+            IReadOnlyCollection<IHookDescriptor> descriptors)
+        {
+            foreach (var descriptor in descriptors)
+            {
+                var hookType = descriptor.HookType;
+
+                var resolveFunc = new Func<object?>(() => _serviceProvider.GetService(hookType));
+
+                yield return new(() =>
+                {
+                    var hook = resolveFunc();
+
+                    if (hook is null)
+                    {
+                        throw new NotResolvedException(hookType);
+                    }
+
+                    return hook as IPostHandleHook<TMessage>;
+                });
+            }
         }
 
         public ILazyReadOnlyCollection<IMessageHandler<TMessage, TMessageResult>> Handlers { get; }
 
-        public ILazyReadOnlyCollection<IPreHandleHook<TMessage>> PostHandleHooks { get; }
+        public ILazyReadOnlyCollection<IPostHandleHook<TMessage>> PostHandleHooks { get; }
 
         public ILazyReadOnlyCollection<IPreHandleHook<TMessage>> PreHandleHooks { get; }
     }
