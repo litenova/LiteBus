af6c2e03a8e840c32bd7b9cf64a356b02e6884c5
Mohammad ShafieZadeh <m.shafiezadeh@outlook.com>
2021-08-09 17:50:09 +0430

    fix command global pre handle hook

--- src/LiteBus.Commands.Abstractions/ICommandPreHandleAsyncHook.cs
+++ src/LiteBus.Commands.Abstractions/ICommandPreHandleAsyncHook.cs
@@ -5,14 +5,14 @@ namespace LiteBus.Commands.Abstractions
     /// <summary>
     ///     Represents an action that is executed on each command pre-handle phase
     /// </summary>
-    public interface ICommandPreHandleAsyncHook : IPreHandleAsyncHook<ICommand>
+    public interface ICommandPreHandleAsyncHook : IPreHandleAsyncHook<ICommandBase>
     {
     }
 
     /// <summary>
     ///     Represents an action that is executed on <typeparamref cref="TCommand" /> pre-handle phase
     /// </summary>
-    public interface ICommandPreHandleAsyncHook<in TCommand> : IPreHandleAsyncHook<TCommand> where TCommand : ICommand
+    public interface ICommandPreHandleAsyncHook<in TCommand> : IPreHandleAsyncHook<TCommand> where TCommand : ICommandBase
     {
     }
 }
