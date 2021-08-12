dc738e269abf57973eeacba28f78a49d9916d779
A. Shafie <Litenova@outlook.com>
2021-08-12 13:19:09 +0430

    add query, event hooks

---
+++ src/LiteBus.Queries.Abstractions/IQueryPostHandleAsyncHook.cs
@@ -0,0 +1,18 @@
+﻿using LiteBus.Messaging.Abstractions;
+
+namespace LiteBus.Queries.Abstractions
+{
+    /// <summary>
+    ///     Represents an action that is executed on each query post-handle phase
+    /// </summary>
+    public interface IQueryPostHandleAsyncHook : IPostHandleAsyncHook<IQueryBase>
+    {
+    }
+
+    /// <summary>
+    ///     Represents an action that is executed on <typeparamref cref="TQuery" /> post-handle phase
+    /// </summary>
+    public interface IQueryPostHandleAsyncHook<in TQuery> : IPostHandleAsyncHook<TQuery> where TQuery : IQueryBase
+    {
+    }
+}
