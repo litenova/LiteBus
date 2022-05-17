02bd00b3b7207cd13962d4be7e4f462d7e0507d0
A. Shafie <Litenova@outlook.com>
2022-05-18 01:05:05 +0200

    Separate generic classes into their own files in LiteBus.Messaging.Abstractions project

--- src/LiteBus.Messaging.Abstractions/ISyncMessageHandler[TMessage, TMessageResult].cs
+++ src/LiteBus.Messaging.Abstractions/ISyncMessageHandler[TMessage, TMessageResult].cs
@@ -4,27 +4,6 @@
 ///     Represents a synchronous message handler
 /// </summary>
 /// <typeparam name="TMessage">the type of message</typeparam>
-/// <remarks>The message can be of any type</remarks>
-public interface ISyncMessageHandler<in TMessage> : IMessageHandler<TMessage, VoidMessageResult>
-{
-    VoidMessageResult IMessageHandler<TMessage, VoidMessageResult>.Handle(IHandleContext<TMessage> context)
-    {
-        Handle(context.Message);
-        return new VoidMessageResult();
-    }
-
-    /// <summary>
-    ///     Handles a message
-    /// </summary>
-    /// <param name="message">the message</param>
-    /// <returns>the message result</returns>
-    void Handle(TMessage message);
-}
-
-/// <summary>
-///     Represents a synchronous message handler
-/// </summary>
-/// <typeparam name="TMessage">the type of message</typeparam>
 /// <typeparam name="TMessageResult">the type of message</typeparam>
 /// <remarks>The message can be of any type</remarks>
 public interface ISyncMessageHandler<in TMessage, out TMessageResult> : IMessageHandler<TMessage, TMessageResult>
