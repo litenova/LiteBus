c4da206145dcafd74b29b8bc1abbf3567d8bac24
Mohammad Shafie-Zadeh <m.shafiezadeh@outlook.com>
2022-05-18 00:09:55 +0200

    Separate generic classes into their own files in LiteBus.Queries.Abstractions.csproj

---
+++ src/LiteBus.Queries.Abstractions/IQueryPreHandler[TQuery].cs
@@ -0,0 +1,11 @@
+﻿using LiteBus.Messaging.Abstractions;
+
+namespace LiteBus.Queries.Abstractions;
+
+/// <summary>
+///     Represents an action that is executed on <typeparamref cref="TQuery" /> pre-handle phase
+/// </summary>
+public interface IQueryPreHandler<in TQuery> : IQueryPreHandlerBase, IMessagePreHandler<TQuery>
+    where TQuery : IQueryBase
+{
+}
