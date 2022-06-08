9acd6b7350edab66b6d386442f859aaac7c22783
Mohammad Shafie-Zadeh <m.shafiezadeh@outlook.com>
2022-06-08 10:45:04 +0200

    remove unused ISyncMessageHandler[TMessage, TMessageResult].cs and ISyncMessageHandler[TMessage].cs files

--- src/LiteBus.Queries.Abstractions/IQuery[TQueryResult].cs
+++ src/LiteBus.Queries.Abstractions/IQuery[TQueryResult].cs
@@ -4,6 +4,7 @@
 ///     Represents a query
 /// </summary>
 /// <typeparam name="TQueryResult">The result type of query</typeparam>
+// ReSharper disable once UnusedTypeParameter
 public interface IQuery<TQueryResult> : IQueryBase
 {
 }
--- src/LiteBus.Queries.Abstractions/IStreamQuery[TQueryResult].cs
+++ src/LiteBus.Queries.Abstractions/IStreamQuery[TQueryResult].cs
@@ -4,6 +4,7 @@ namespace LiteBus.Queries.Abstractions;
 ///     Represents a query
 /// </summary>
 /// <typeparam name="TQueryResult">The result type of query</typeparam>
+// ReSharper disable once UnusedTypeParameter
 public interface IStreamQuery<out TQueryResult> : IQueryBase
 {
 }
