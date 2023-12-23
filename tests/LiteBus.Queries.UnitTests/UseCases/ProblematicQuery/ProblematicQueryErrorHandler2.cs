bc5f3a33ddcac10db9b61fae3200e039acc25e65
litenova <litenova@outlook.com>
2023-12-24 00:17:03 +0100

    Fix the missing `Exception` parameter in `IAsyncMessageErrorHandler[TMessage, TMessageResult]` and `IAsyncMessageErrorHandler[TMessage]` interfaces.

--- tests/LiteBus.Queries.UnitTests/UseCases/ProblematicQuery/ProblematicQueryErrorHandler2.cs
+++ tests/LiteBus.Queries.UnitTests/UseCases/ProblematicQuery/ProblematicQueryErrorHandler2.cs
@@ -4,7 +4,7 @@ namespace LiteBus.Queries.UnitTests.UseCases.ProblematicQuery;
 
 public sealed class ProblematicQueryErrorHandler2 : IQueryErrorHandler<ProblematicQuery>
 {
-    public Task HandleErrorAsync(ProblematicQuery message, object messageResult, CancellationToken cancellationToken = default)
+    public Task HandleErrorAsync(ProblematicQuery message, object? messageResult, Exception exception, CancellationToken cancellationToken = default)
     {
         message.ExecutedTypes.Add(GetType());
 
