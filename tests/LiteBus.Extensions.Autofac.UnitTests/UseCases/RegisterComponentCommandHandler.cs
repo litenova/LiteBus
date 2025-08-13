41655154f044f8f7a0bc6bfacb0450886a3948d4
a.shafie <ashfzd@pm.me>
2025-08-13 11:40:28 +0200

    Add AutoFac

---
+++ tests/LiteBus.Extensions.Autofac.UnitTests/UseCases/RegisterComponentCommandHandler.cs
@@ -0,0 +1,15 @@
+using LiteBus.Commands.Abstractions;
+
+namespace LiteBus.Extensions.Autofac.UnitTests.UseCases;
+
+/// <summary>
+/// The main handler for <see cref="RegisterComponentCommand"/>.
+/// </summary>
+public sealed class RegisterComponentCommandHandler : ICommandHandler<RegisterComponentCommand>
+{
+    public Task HandleAsync(RegisterComponentCommand message, CancellationToken cancellationToken = default)
+    {
+        message.ExecutedHandlers.Add(GetType());
+        return Task.CompletedTask;
+    }
+}
